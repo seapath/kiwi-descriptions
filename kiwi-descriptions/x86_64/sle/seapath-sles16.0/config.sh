53759ca8403d8e176f1370006a82f444407bbed7
Elinor Montmasson <elinor.montmasson@savoirfairelinux.com>
2026-04-24 16:55:23 +0200

    .cqfdrc: seapath-sle16.0: add SLES 16.0 appliance
    Create a new KIWI-NG image appliance for SEAPATH SLES 16.0 based images.
    The image appliance was copied from the SEAPATH SLE RT 15 SP7
    appliance and adapted for SLES 16.0.
    
    Major adaptations for SLES 16.0:
    * In SLES 16.0 the linux binary (vmlinuz) is installed in /usr/lib.
      /boot now only contains a symlink to this file.
      Therefore, the boot partition must be disabled, otherwise vmlinuz
      can't be loaded by grub because no installed in the boot partition.
    * Fix qemu packages version to 10.0.8 because some of these packages
      come from the Package Hub in version 10.0.9 which conflicts with the
      other QEMU packages.
    * The "libvirt" package group doesn't exist anymor. Instead, individual
      "libvirt-*" packages must be specified.
    * Compared to SLE RT 15 SP7, SLES 16.0 has access to all SLE Python 3
      related packages. Python modules that can be installed from SLE
      repositories are moved back to appliance.kiwi.
      Note that podman-compose must still be installed with pip.
    * Python was update to version 3.13, so package names were adapted.
    * Set up SEAPATH related image profiles based on package separation
      already done in other SEAPATH flavors. For now, only hypervisor
      related packages are using the profile feature.
    
    Signed-off-by: Elinor Montmasson <elinor.montmasson@savoirfairelinux.com>

---
+++ kiwi-descriptions/x86_64/sle/seapath-sles16.0/config.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -ex
+
+# Kiwi utility functions
+test -f /.kconfig && . /.kconfig
+
+# An empty machine-id doesn't mean first boot for systemd
+# Should be set to "uninitialized"
+echo "uninitialized" > /etc/machine-id
+
+
+# Additional python packages
+python3.13 -m pip install \
+    podman-compose
+
+# Configure SSH
+
+chown admin:admin /home/admin/.ssh/authorized_keys
+chown ansible:ansible /home/ansible/.ssh/authorized_keys
+
+chmod 0600 /home/admin/.ssh/authorized_keys
+chmod 0600 /home/ansible/.ssh/authorized_keys
+
+baseService sshd on
+
+# Network
+
+baseService systemd-networkd on
+baseService systemd-networkd-wait-online off
+baseService systemd-resolved on
