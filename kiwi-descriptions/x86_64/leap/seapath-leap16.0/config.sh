a5eb3adbf9e8723c5a4a417e6f2fdec58e63fc58
Victor Westerhuis <victor.westerhuis@alliander.com>
2026-07-10 13:50:22 +0200

    Add an openSUSE Leap 16.0 variant
    Signed-off-by: Victor Westerhuis <victor.westerhuis@alliander.com>

---
+++ kiwi-descriptions/x86_64/leap/seapath-leap16.0/config.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+# Copyright (C) 2026 Savoir-faire Linux, Inc.
+# SPDX-License-Identifier: Apache-2.0
+
+set -ex
+
+# Kiwi utility functions & variables
+test -f /.kconfig && . /.kconfig
+test -f /.profile && . /.profile
+
+# An empty machine-id doesn't mean first boot for systemd
+# Should be set to "uninitialized"
+echo "uninitialized" > /etc/machine-id
+
+
+# Configure pam
+
+install -m 0440 /usr/lib/pam.d/login /etc/pam.d/login
+
+for i in /etc/pam.d/common-* /etc/pam.d/postlogin-*; do
+    # Disable pam-config managed files if found
+    if [ -L "$i" ] && [ "$(readlink $i)" = "${i}-pc" ]; then
+        rm $i
+        sed '/^#.*/d' ${i}-pc > $i;
+    fi
+done
+
+# Configure user permissions
+
+chown -R admin:admin /home/admin
+chown -R ansible:ansible /home/ansible
+
+if [ -f /home/admin/.ssh/authorized_keys ]; then
+    chmod 0600 /home/admin/.ssh/authorized_keys
+else
+    mkdir -p /home/admin/.ssh
+fi
+if [ -f /home/ansible/.ssh/authorized_keys ]; then
+    chmod 0600 /home/ansible/.ssh/authorized_keys
+else
+    mkdir -p /home/ansible/.ssh
+fi
+
+chmod 0440 /etc/sudoers
+chmod 0440 /etc/sudoers.d/ansible
+
+# Configure SSH
+
+baseService sshd on
+
+# Additional configurations
+
+mv /usr/share/vim/vim92/suse.vimrc /etc/vimrc
+
+# Network
+
+baseService systemd-networkd on
+baseService systemd-networkd-wait-online off
+baseService systemd-resolved on
+
+# Open vSwitch
+
+baseService openvswitch on
+
+cat >> /etc/sysconfig/openvswitch <<EOF
+# Disable running openvswitch with the openvswitch user/group
+OVS_USER_ID="root:root"
+EOF
+
+# Disable modular libvirt daemons, SEAPATH uses the monolithic libvirtd daemon
+
+baseService virtnetworkd off
+baseService virtnodedevd off
+baseService virtnwfilterd off
+baseService virtproxyd off
+baseService virtqemud off
+baseService virtsecretd off
+baseService virtstoraged off
+
+# logrotate
+
+if echo "${kiwi_profiles}" | grep -qw "cluster"; then
+    rm -f /etc/logrotate.d/cephadm
+fi
---
+++ kiwi-descriptions/x86_64/leap/seapath-leap16.0/post_bootstrap.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Copyright (C) 2026 Savoir-faire Linux, Inc.
+# SPDX-License-Identifier: Apache-2.0
+
+set -ex
+
+mkdir -p /var/tmp/kiwi_containers/ceph
+mkdir -p /var/tmp/kiwi_containers/library
